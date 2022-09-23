0aa2dd5edbf6d8c5af3489a184a83561c3567237
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2022-09-23 18:11:49 +0300

    odev4 ve odev5 eklendi

---
+++ Odevler/odev4/odev4/odev4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region devrim_mehmet_pattabanoglu-odev4
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i =1; i < 6; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                if (i==5) {
+                    for (int k = 0; k < 24; k++)
+                    {
+                        Console.Write("-");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 1; i < 6; i++)
+            {
+                for (int k = 5; k > i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+               
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ Odevler/odev5/odev5/odev5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Devrim_Mehmet_Pattabanoglu-odev5
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 5; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int w = 0; w < i-1; w++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
