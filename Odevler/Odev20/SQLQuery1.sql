9955a8deb0e376facc2731532647bd6645f5699e
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2022-10-20 22:48:56 +0300

    odev20

--- Odevler/Odev20/SQLQuery1.sql
+++ Odevler/Odev20/SQLQuery1.sql
@@ -4,11 +4,12 @@
 --with schemabinding,encryption)
 go
 create view Odev
+with schemabinding,encryption
 as
-select   p.ProductName,p.UnitPrice,(p.UnitPrice*1.18) as 'KDV DAHÝL',c.CategoryName,s.Country,UnitsInStock from Categories c 
-inner join Products p on p.CategoryID=c.CategoryID
-inner join Suppliers s on s.SupplierID=p.SupplierID
-inner join [Order Details] oD on od.ProductID=p.ProductID
+select   p.ProductName,p.UnitPrice,(p.UnitPrice*1.18) as 'KDV DAHÝL',c.CategoryName,s.Country,UnitsInStock from dbo.Categories c 
+inner join dbo.Products p on p.CategoryID=c.CategoryID
+inner join dbo.Suppliers s on s.SupplierID=p.SupplierID
+inner join dbo.[Order Details] oD on od.ProductID=p.ProductID
 where c.CategoryName= 'Beverages' and s.Country='USA' and UnitsInStock is not null
 group by p.ProductName,p.UnitPrice,c.CategoryName,s.Country,p.UnitsInStock
 
